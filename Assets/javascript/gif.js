841cf5472897fec67eab96810f200ea647520e95
Fabian Flores <ffflores1@outlook.com>
2019-04-09 21:33:31 -0400

    refactored code to get MVP

--- Assets/javascript/gif.js
+++ Assets/javascript/gif.js
@@ -7,80 +7,54 @@ function makeButtons (){
   $('#buttonsView').empty();
 
   for (var i = 0; i < brandsArray.length; i++) {
-  
+
     var a=$('<button>')
     a.addClass('brand');
-    a.attr('data-name', brand[i]);
-    a.text(brand[i]);
+    a.attr('data-name', brandsArray[i]);
+    a.text(brandsArray[i]);
 
     $('#buttonsView').append(a)
-    
   }
 
 }
 
-var currimage = 0;
-window.onload = () => {
-    const factsArr = [
-    {
-    image: 
-    'https://media.giphy.com/media/FtOtVWA4Ozo8E/giphy.gif',
-    }
-  ];document.getElementById('generate-btn').addEventListener('click', () => {
-      
-    document.getElementById('image').setAttribute('src', factsArr[currImage].image);
-     currImage++;
-     if (currImage == factsArr.length)
-        currImage  = 0;
-   })
-  }
 
-  <button id="generate-btn">Balenciaga</button>
-  <img id="image"></img>
-
-
-      
-$("#addbrand").on("click"),function() {
-
+$("#add-brand").on("click",function(event) {
+  event.preventDefault();
   var brand = $("#brand-input").val().trim();
   brandsArray.push(brand);
-  $("#got-input").val("");
+  // $("#got-input").val("");
+  // clear brand input
+  $("#brand-input").val("");
 
   makeButtons();
-
-  console.log(brands);
-
-  return false;
-
-}
+});
 
 makeButtons();
 
 function displayGifs(){
        var brand = $(this).attr("data-name");
-       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + fashionbrands + "&api_key=dc6zaTOxFJmzC";
+       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + brand + "&api_key=dc6zaTOxFJmzC";
 
        $.ajax({url: queryURL, method: "GET"}).done(function (response) {
         console.log(response.data);
         var results = response.data;
-    
+
         for (var i = 0; i < results.length; i++) {
-          
-          var gifDiv = $('<div class=gifs>');
+
+          // var gifDiv = $('<div class=gifs>');
           var brandGif = $('<img>');
-            brandGif.attr('src', results[i].images.fixed_height_still.url);
-            brandGif.attr('title', "Rating: " + results[i].rating);
-            brandGif.attr('data-still', results[i].images.fixed_height_still.url);
-            brandGif.attr('data-state', 'still');
-            brandGif.addClass('gif');
-            brandGif.attr('data-animate', results[i].images.fixed_height.url);
-          
-          brandDiv.append(brandGif)
-        
-  
-          $("#gifsView").prepend(gifDiv);
+
+          brandGif.attr('src', results[i].images.fixed_height_still.url);
+          brandGif.attr('title', "Rating: " + results[i].rating);
+          brandGif.attr('data-still', results[i].images.fixed_height_still.url);
+          brandGif.attr('data-state', 'still');
+          brandGif.addClass('gif');
+          brandGif.attr('data-animate', results[i].images.fixed_height.url);
+
+          $("#gifs-appear-here").prepend(brandGif);
         }
-        
+
       });
   }
 
@@ -94,19 +68,8 @@ function displayGifs(){
                   $(this).attr('data-state', 'still');
               };
   });
-  
-  
-  
+
+
+
 
   $(document).on("click", ".brand", displayGifs);
-  
-  
-
-
- 
-
-
-
-
-
-
