da77aad8a31346294ef85ea6ba3ffeaf1ccfe8bb
shreya suman <shreyasumanck@gmail.com>
2019-08-13 14:08:30 -0400

    Initial commit

--- Assets/javascript/gif.js
+++ Assets/javascript/gif.js
@@ -1,112 +1,91 @@
-//Initial brand array
+$(document).ready(function() {
 
-var brandsArray = ["Chanel", "Louis Vuitton", "Christian Dior SE", "Saint Laurent", "Balenciaga", "Balmain", "Gucci", "Givenchy"];
+  var animals = [
+    "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
+    "bird", "ferret", "turtle", "sugar glider", "chinchilla",
+    "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
+    "capybara", "teacup pig", "serval", "salamander", "frog"
+  ];
 
-function makeButtons (){
+  // function to make buttons and add to page
+  function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+    $(areaToAddTo).empty();
 
-  $('#buttonsView').empty();
-
-  for (var i = 0; i < brandsArray.length; i++) {
-  
-    var a=$('<button>')
-    a.addClass('brand');
-    a.attr('data-name', brand[i]);
-    a.text(brand[i]);
-
-    $('#buttonsView').append(a)
-    
-  }
-
-}
-
-var currimage = 0;
-window.onload = () => {
-    const factsArr = [
-    {
-    image: 
-    'https://media.giphy.com/media/FtOtVWA4Ozo8E/giphy.gif',
+    for (var i = 0; i < arrayToUse.length; i++) {
+      var a = $("<button>");
+      a.addClass(classToAdd);
+      a.attr("data-type", arrayToUse[i]);
+      a.text(arrayToUse[i]);
+      $(areaToAddTo).append(a);
     }
-  ];document.getElementById('generate-btn').addEventListener('click', () => {
-      
-    document.getElementById('image').setAttribute('src', factsArr[currImage].image);
-     currImage++;
-     if (currImage == factsArr.length)
-        currImage  = 0;
-   })
+
   }
 
-  <button id="generate-btn">Balenciaga</button>
-  <img id="image"></img>
+  $(document).on("click", ".animal-button", function() {
+    $("#animals").empty();
+    $(".animal-button").removeClass("active");
+    $(this).addClass("active");
 
+    var type = $(this).attr("data-type");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-      
-$("#addbrand").on("click"),function() {
-
-  var brand = $("#brand-input").val().trim();
-  brandsArray.push(brand);
-  $("#got-input").val("");
-
-  makeButtons();
-
-  console.log(brands);
-
-  return false;
-
-}
-
-makeButtons();
-
-function displayGifs(){
-       var brand = $(this).attr("data-name");
-       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + fashionbrands + "&api_key=dc6zaTOxFJmzC";
-
-       $.ajax({url: queryURL, method: "GET"}).done(function (response) {
-        console.log(response.data);
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
         var results = response.data;
-    
+
         for (var i = 0; i < results.length; i++) {
-          
-          var gifDiv = $('<div class=gifs>');
-          var brandGif = $('<img>');
-            brandGif.attr('src', results[i].images.fixed_height_still.url);
-            brandGif.attr('title', "Rating: " + results[i].rating);
-            brandGif.attr('data-still', results[i].images.fixed_height_still.url);
-            brandGif.attr('data-state', 'still');
-            brandGif.addClass('gif');
-            brandGif.attr('data-animate', results[i].images.fixed_height.url);
-          
-          brandDiv.append(brandGif)
-        
-  
-          $("#gifsView").prepend(gifDiv);
+          var animalDiv = $("<div class=\"animal-item\">");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var animated = results[i].images.fixed_height.url;
+          var still = results[i].images.fixed_height_still.url;
+
+          var animalImage = $("<img>");
+          animalImage.attr("src", still);
+          animalImage.attr("data-still", still);
+          animalImage.attr("data-animate", animated);
+          animalImage.attr("data-state", "still");
+          animalImage.addClass("animal-image");
+
+          animalDiv.append(p);
+          animalDiv.append(animalImage);
+
+          $("#animals").append(animalDiv);
         }
-        
       });
-  }
-
-  $(document).on('click', '.gif', function(){
-    var state = $(this).attr('data-state');
-      if ( state == 'still'){
-                  $(this).attr('src', $(this).data('animate'));
-                  $(this).attr('data-state', 'animate');
-              }else{
-                  $(this).attr('src', $(this).data('still'));
-                  $(this).attr('data-state', 'still');
-              };
   });
-  
-  
-  
 
-  $(document).on("click", ".brand", displayGifs);
-  
-  
+  $(document).on("click", ".animal-image", function() {
 
+    var state = $(this).attr("data-state");
 
- 
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    }
+    else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
 
+  $("#add-animal").on("click", function(event) {
+    event.preventDefault();
+    var newAnimal = $("input").eq(0).val();
 
+    if (newAnimal.length > 2) {
+      animals.push(newAnimal);
+    }
 
+    populateButtons(animals, "animal-button", "#animal-buttons");
 
+  });
 
-
+  populateButtons(animals, "animal-button", "#animal-buttons");
+});
